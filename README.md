2dc082147796d670855354ca6aa64cc00d8b5c9b
Chunpeng Huo <huocp@me.com>
2019-07-08 12:29:56 +1000

    feat: basic webpack ts/js apps

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# new
-The scaffolding repo for Aurelia vNext used by the makes tool to setup new projects.
+# Aurelia 2 Scaffolding skeleton
+
+_Work In Progress_
+
+The scaffolding repo for Aurelia 2 used by the [makes](https://makes.js.org) tool to create new Aurelia 2 projects.
+
+## Create an Aurelia 2 project
+
+There is zero-installation, just do:
+
+```bash
+npx makes aureila
+```
+
+> Note makes requires Node.js v8.9.0 or above.
+
+## Status
+
+Just basic app with one of bundlers: webpack, parcel, browserify, FuseBox, dumber (successor of CLI built-in bundler).
+
+No unit tests, no e2e tests.
+
+Require verbose decorators on all resources, no support of convention (Aurelia v1 like) yet.
---
+++ before.js
@@ -0,0 +1,39 @@
+// Use "before" task to ask user to select a preset (to skip questionnaire).
+
+const PRESETS = {
+  'default-esnext': ['webpack', 'babel'],
+  'default-typescript': ['webpack', 'typescript'],
+};
+
+module.exports = async function({unattended, prompts, predefinedProperties}) {
+  // don't ask when running in silent mode.
+  if (unattended) return;
+
+  const preset = await prompts.select({
+    message: 'Would you like to use the default setup or customize your choices?',
+    choices: [
+      {
+        value: 'default-esnext',
+        title: 'Default ESNext Aurelia 2 App',
+        hint: 'A basic Aurelia 2 App with Babel and Webpack'
+      }, {
+        value: 'default-typescript',
+        title: 'Default TypeScript Aurelia 2 App',
+        hint: 'A basic Aurelia 2 App with TypeScript and Webpack'
+      }, {
+        title: 'Custom Aurelia 2 App',
+        hint: 'Select bundler, transpiler, and more.'
+      }
+    ]
+  });
+
+  if (preset) {
+    const preselectedFeatures = PRESETS[preset];
+    if (preselectedFeatures) {
+      return {
+        silentQuestions: true, // skip following questionnaire
+        preselectedFeatures
+      };
+    }
+  }
+};
