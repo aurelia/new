c8c4c4232958d5cf300012b3d2ef0a59e57f57f7
Chunpeng Huo <huocp@me.com>
2020-05-08 12:48:03 +1000

    test: try npm

--- e2e-test.js
+++ e2e-test.js
@@ -130,8 +130,8 @@ skeletons.forEach((features, i) => {
     t.pass('made skeleton');
     process.chdir(appFolder);
 
-    console.log('-- yarn');
-    await run('yarn');
+    console.log('-- npm i');
+    await run('npm i');
     t.pass('installed deps');
 
     console.log('-- npm test');
