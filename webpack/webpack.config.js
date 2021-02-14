d71c0ee3af74722e41e55fc5a9a01e5f0272c998
Brandon seydel <brandonseydel@hotmail.com>
2021-02-13 22:44:31 -0600

    Update webpack.config.js
    No devtool for production builds.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -52,7 +52,7 @@ module.exports = function(env, { /* @if jasmine || tape || mocha*/runTest, /* @e
   // @endif
   return {
     mode: production ? 'production' : 'development',
-    devtool: production ? 'source-map' : 'inline-source-map',
+    devtool: production ? undefined : 'inline-source-map',
     // @if jasmine || tape || mocha
     entry: test ? './test/all-spec./* @if babel */js/* @endif *//* @if typescript */ts/* @endif */' :  './src/main./* @if babel */js/* @endif *//* @if typescript */ts/* @endif */',
     // @endif
